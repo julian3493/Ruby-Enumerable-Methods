02fdd2bdb7b85561991255abe8086d68ef62636f
alejandrotoledoweb <atoledofr@gmail.com>
2020-10-15 17:33:49 -0500

    added my_none enumerable

--- methods.rb
+++ methods.rb
@@ -59,7 +59,33 @@ module Enumerable
     end
     false
   end
+
+  def my_none?(*arg)
+    if block_given?
+      my_each { |element| return false if yield(element) }
+      true
+    elsif arg.is_a?(Class)
+      my_each { |element| return false if element.class == arg }
+      true
+    elsif arg.class == Regexp
+      my_each { |element| return false if arg.match(element) }  
+    elsif self.length.zero? || self.nil?
+      true
+    elsif !block_given? && arg.nil?
+      my_any? { |element| return false if element == true }
+      my_each { |element| return true if element.nil? || element == false }
+    else
+      my_each { |element| return false if element == arg }
+    end
+    true
+  end
   
+  
+  
+
+    
+        
+
 end
 
 # rubocop:enable Style/CaseEquality
