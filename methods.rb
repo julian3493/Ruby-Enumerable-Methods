79322366413043ef701fe23c371de2ad4c6dc5d6
alejandrotoledoweb <atoledofr@gmail.com>
2020-10-15 16:33:41 -0500

    added my_each_with_index enumerable method

--- methods.rb
+++ methods.rb
@@ -5,13 +5,28 @@ module Enumerable
     return to_enum(:my_each) unless block_given?
 
     list = to_a if self.class == Range || Hash
-    i=0
-    while i<list.length
+    i = 0
+    while i < list.length
       yield(list[i])
       i += 1
     end
     self
   end
+
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
+    list = to_a if self.class == Range || Hash
+    i = 0
+    while i < list.length
+      yield(list[i], i)
+      i += 1
+    end
+    self
+  end
+
+
+
 end
 
 # rubocop:enable Style/CaseEquality
